b112a2b5a4928e8e1cecd81d2cdf4654a588ab44
Manish Mandal <manish.mandal@layrins.com>
2022-06-19 01:19:13 +0530

    added tailwindcss

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,19 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
-import Auth from './components/features/auth/Auth'
-import axios from 'axios'
+import { useState } from 'react';
+import './App.css';
+import Auth from './components/features/auth/Auth';
+import axios from 'axios';
 
-axios.defaults.baseURL = 'http://192.168.105.253:8000'
+axios.defaults.baseURL = 'http://192.168.105.253:8000';
 // axios.defaults.baseURL = 'https://task-app.heroku.app'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-     <Auth />
+    <div className='App'>
+      <Auth />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
