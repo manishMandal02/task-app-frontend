0ffae6eef5dfdb6e33442c6c6cea85023fca3e7a
Manish Mandal <manish.mandal@layrins.com>
2022-06-18 23:21:13 +0530

    added axios with base url

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,10 @@ import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Auth from './components/features/auth/Auth'
+import axios from 'axios'
+
+axios.defaults.baseURL = 'http://192.168.105.253:8000'
+// axios.defaults.baseURL = 'https://task-app.heroku.app'
 
 function App() {
   const [count, setCount] = useState(0)
